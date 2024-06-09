a0f3603b47dc20c6c1e7cf81f1f5849b31d1a18a
Léonard Bise <leonard.bise@gmail.com>
2024-06-09 17:05:44 +0200

    Chapter 7

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Port 5000 is used by AirPlay Receiver
-flask run -p 4999
+FLASK_DEBUG=1 MAIL_SERVER=localhost MAIL_PORT=8025 flask run -p 4999
---
+++ start_email.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+aiosmtpd -n -c aiosmtpd.handlers.Debugging -l localhost:8025
